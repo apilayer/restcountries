e158e0c3c08de2139f9724ee507a6f77b9e08b2a
fayder <fayder.florez@gmail.com>
2013-12-28 21:26:57 +0200

    RestCountries version 1.0.0 First Commit

---
+++ src/test/java/vaeke/restcountries/v1/CountryRestTest.java
@@ -0,0 +1,135 @@
+package vaeke.restcountries.v1;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import vaeke.restcountries.v1.domain.DesCountry;
+
+import com.eclipsesource.restfuse.Assert;
+import com.eclipsesource.restfuse.Destination;
+import com.eclipsesource.restfuse.HttpJUnitRunner;
+import com.eclipsesource.restfuse.Method;
+import com.eclipsesource.restfuse.Response;
+import com.eclipsesource.restfuse.annotation.Context;
+import com.eclipsesource.restfuse.annotation.HttpTest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+@RunWith(HttpJUnitRunner.class)
+public class CountryRestTest {
+	
+	@Rule
+	public Destination destination = new Destination(this, "http://localhost:8081/rest/v1");
+	
+	@Context
+	private Response response;
+
+	@HttpTest(method = Method.GET, path = "/")
+	public void checkOnlineStatus() {
+		Assert.assertOk(response);
+	}
+	
+	@HttpTest(method = Method.GET, path = "/alpha/co")
+	public void alpha2() {
+		Assert.assertOk(response);
+		DesCountry country = deserialize(response.getBody());
+		org.junit.Assert.assertEquals("CO", country.getAlpha2Code());
+	}
+	
+	@HttpTest(method = Method.GET, path = "/alpha/col")
+	public void alpha3() {
+		Assert.assertOk(response);
+		DesCountry country = deserialize(response.getBody());
+		org.junit.Assert.assertEquals("COL", country.getAlpha3Code());
+	}
+	
+	@HttpTest(method = Method.GET, path = "/currency/eur")
+	public void currency() {
+		Assert.assertOk(response);
+		List<DesCountry> countries = deserializeList(response.getBody());
+		org.junit.Assert.assertFalse(countries.isEmpty());
+		for(DesCountry country : countries) {
+			org.junit.Assert.assertTrue(country.getCurrencies().contains("EUR"));
+		}
+	}
+	
+	@HttpTest(method = Method.GET, path = "/callingcode/1")
+	public void callingcode() {
+		Assert.assertOk(response);
+		List<DesCountry> countries = deserializeList(response.getBody());
+		org.junit.Assert.assertFalse(countries.isEmpty());
+		for(DesCountry country : countries) {
+			org.junit.Assert.assertTrue(country.getCallingCodes().contains("1"));
+		}
+	}
+	
+	@HttpTest(method = Method.GET, path = "/capital/washington")
+	public void capital() {
+		Assert.assertOk(response);
+		List<DesCountry> countries = deserializeList(response.getBody());
+		org.junit.Assert.assertFalse(countries.isEmpty());
+		for(DesCountry country : countries) {
+			org.junit.Assert.assertEquals("washington d.c.", country.getCapital().toLowerCase());
+		}
+	}
+	
+	@HttpTest(method = Method.GET, path = "/region/europe")
+	public void region() {
+		Assert.assertOk(response);
+		List<DesCountry> countries = deserializeList(response.getBody());
+		for(DesCountry country : countries) {
+			org.junit.Assert.assertEquals("europe", country.getRegion().toLowerCase());
+		}
+	}
+	
+	@HttpTest(method = Method.GET, path = "/lang/et")
+	public void language() {
+		Assert.assertOk(response);
+		List<DesCountry> countries = deserializeList(response.getBody());
+		for(DesCountry country : countries) {
+			org.junit.Assert.assertTrue(country.getLanguages().contains("et"));
+			System.out.println(country.getName());
+		}
+	}
+	
+	@HttpTest(method = Method.GET, path = "/alpha?codes=ar;be;fr;it")
+	public void alphaList() {
+		Assert.assertOk(response);
+		List<DesCountry> countries = deserializeList(response.getBody());
+		org.junit.Assert.assertFalse(countries.isEmpty());
+		org.junit.Assert.assertEquals(4, countries.size());
+		org.junit.Assert.assertTrue(response.getBody().contains("\"alpha2Code\":\"AR\""));
+		org.junit.Assert.assertTrue(response.getBody().contains("\"alpha2Code\":\"BE\""));
+		org.junit.Assert.assertTrue(response.getBody().contains("\"alpha2Code\":\"FR\""));
+		org.junit.Assert.assertTrue(response.getBody().contains("\"alpha2Code\":\"IT\""));
+	}
+	
+	@HttpTest(method = Method.GET, path = "/name/russia")
+	public void name() {
+		Assert.assertOk(response);
+		List<DesCountry> countries = deserializeList(response.getBody());
+		org.junit.Assert.assertFalse(countries.isEmpty());
+		for(DesCountry c : countries) {
+			org.junit.Assert.assertEquals("Russia", c.getName());
+		}
+		
+	}
+	
+	private DesCountry deserialize(String json) {
+		Gson gson = new Gson();
+		DesCountry country = gson.fromJson(json, DesCountry.class);
+		return country;
+	}
+	
+	private List<DesCountry> deserializeList(String json) {
+		Gson gson = new Gson();
+		Type listType = new TypeToken<ArrayList<DesCountry>>() {}.getType();
+		List<DesCountry> countries = gson.fromJson(json, listType);
+		return countries;
+	}
+
+}
---
+++ src/test/java/vaeke/restcountries/v1/EmptyDataTest.java
@@ -0,0 +1,63 @@
+package vaeke.restcountries.v1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import vaeke.restcountries.v1.domain.Country;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+public class EmptyDataTest {
+	
+List<Country> countries;
+	
+	@Before
+	public void before() throws IOException {
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream("countriesV1.json");
+		Gson gson = new Gson();
+		JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+		countries = new ArrayList<Country>();
+		reader.beginArray();
+		while(reader.hasNext()) {
+			Country country = gson.fromJson(reader, Country.class);
+			countries.add(country);
+		}
+		reader.endArray();
+        reader.close();
+	}
+	
+	@Test
+	public void emptyPopulation() {
+		List<Country> result = new ArrayList<Country>();
+		for(Country c : countries) {
+			if(c.getPopulation() == null) {
+				result.add(c);
+			}
+		}
+		System.out.println("\nEmpty Population");
+		for(Country c : result) {
+			System.out.println(c.getName());
+		}
+	}
+	
+	@Test
+	public void emptyLanguages() throws Exception {
+		List<Country> result = new ArrayList<Country>();
+		for(Country c : countries) {
+			result.add(c);
+		}
+		System.out.println("\nEmpty Languages");
+		for(Country c : result) {
+			if(c.getLanguagesCodes() == null || c.getLanguagesCodes().isEmpty())
+				System.out.println(c.getName());
+		}
+	}
+
+}
