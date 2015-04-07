f94ff1c5e53ade416767d734809772f7459cd2bb
Fayder <fayder.florez@gmail.com>
2015-04-07 22:13:44 +0300

    Removed ResponseCorsFilter

--- src/main/java/vaeke/restcountries/servlet/NotFoundExceptionMapper.java
+++ src/main/java/vaeke/restcountries/servlet/NotFoundExceptionMapper.java
@@ -8,12 +8,12 @@ import javax.ws.rs.ext.Provider;
 import org.apache.log4j.Logger;
 
 @Provider
-public class NotFoundExceptionMapper implements ExceptionMapper<Throwable> {
+public class NotFoundExceptionMapper implements ExceptionMapper<javax.ws.rs.NotFoundException> {
 	
 	private static final Logger LOG = Logger.getLogger(NotFoundExceptionMapper.class);
 
 	@Override
-	public Response toResponse(Throwable exception) {
+	public Response toResponse(javax.ws.rs.NotFoundException exception) {
 		LOG.error(exception.getMessage());
 		return Response.status(Status.NOT_FOUND).build();
 	}
