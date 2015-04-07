077d24ebeda9bb9c975b07a7d0fa012551e87990
Fayder <fayder.florez@gmail.com>
2015-04-07 10:40:50 +0300

    Temporary JS redirection to HTTPS

--- src/main/webapp/js/restcountries.js
+++ src/main/webapp/js/restcountries.js
@@ -3,6 +3,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 var restc = restc || {};
 
+//if (window.location.protocol != "https:") { 
+//	window.location.href = "https://restcountries.eu"
+//}
+
 $(function () {
 	'use strict';
 	
