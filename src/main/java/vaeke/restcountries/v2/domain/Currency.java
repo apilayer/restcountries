19b48cb5a8944ae583c2695d883316df0b481cfd
Fayder <fayder.florez@gmail.com>
2015-08-22 11:31:17 +0300

    Preparations for REST Countries v2

---
+++ src/main/java/vaeke/restcountries/v2/domain/Currency.java
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package vaeke.restcountries.v2.domain;
+
+public class Currency {
+
+    private String code;
+    private String name;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
